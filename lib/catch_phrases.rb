97ce80f26b4af7d92774fea426a7053ca2c3d6f6
David Cha <david.cha44@qmail.cuny.edu>
2019-11-05 21:20:46 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,4 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
@@ -7,5 +6,6 @@ end
 
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
